65621e01bfd3082820b369902dfe96340d0a8db6
Vincent Batts <vbatts@hashbangbash.com>
2013-06-30 02:24:38 +0000

    adding a few tests to the rss tests

--- channel.go
+++ channel.go
@@ -5,18 +5,18 @@ import (
 )
 
 type Channel struct {
-	XMLName        xml.Name `xml:"channel"`
-  // until Go1.1, to handle DefaultSpace
-  //Atom           Atom     `xml:"atom link"`
-	Title          string   `xml:"title"`
-  Link           string   `xml:"link"`
-	Docs           string   `xml:"docs,omitempty"`
-	Description    string   `xml:"description"`
-	Language       string   `xml:"language,omitempty"`
-	ManagingEditor string   `xml:"managingEditor,omitempty"`
-	WebMaster      string   `xml:"webMaster,omitempty"`
-	PubDate        string   `xml:"pubDate,omitempty"`
-	LastBuildDate  string   `xml:"lastBuildDate,omitempty"`
-	Generator      string   `xml:"generator,omitempty"`
-	Items          []Item   `xml:"item"`
+	XMLName xml.Name `xml:"channel"`
+	// until Go1.1, to handle DefaultSpace
+	//Atom           Atom     `xml:"atom link"`
+	Title          string `xml:"title"`
+	Link           string `xml:"link"`
+	Docs           string `xml:"docs,omitempty"`
+	Description    string `xml:"description"`
+	Language       string `xml:"language,omitempty"`
+	ManagingEditor string `xml:"managingEditor,omitempty"`
+	WebMaster      string `xml:"webMaster,omitempty"`
+	PubDate        string `xml:"pubDate,omitempty"`
+	LastBuildDate  string `xml:"lastBuildDate,omitempty"`
+	Generator      string `xml:"generator,omitempty"`
+	Items          []Item `xml:"item"`
 }
--- rss_test.go
+++ rss_test.go
@@ -2,7 +2,7 @@ package rss
 
 import (
 	"encoding/xml"
-	"fmt"
+	//"fmt"
 	"os"
 	"testing"
 )
@@ -15,5 +15,13 @@ func TestThings(t *testing.T) {
 	if err != nil {
 		t.Errorf("%s", err)
 	}
-	fmt.Printf("%#v\n", rss)
+	//fmt.Printf("%#v\n", rss)
+	exp_str := "SlackBuilds.org ChangeLog"
+	if rss.Channel.Title != exp_str {
+		t.Errorf("title [%s] did not equal %s", rss.Channel.Title, exp_str)
+	}
+	exp_int := 29
+	if len(rss.Channel.Items) != exp_int {
+		t.Errorf("items [%d] did not equal %d", len(rss.Channel.Items), exp_int)
+	}
 }
