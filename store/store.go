57a35d3be5413140f08435638dc179c265a033bb
Vincent Batts <vbatts@redhat.com>
2013-07-05 18:12:19 -0400

    FeedStore now has some functionality

--- store/store.go
+++ store/store.go
@@ -1,33 +1,89 @@
 package store
 
 import (
-  "code.google.com/p/go-sqlite/go1/sqlite3"
-  "os"
+	"code.google.com/p/go-sqlite/go1/sqlite3"
+	"os"
 )
 
 func FileExists(filename string) bool {
-  if _, err := os.Stat(filename); os.IsNotExist(err) {
-    return false
-  }
-  return true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
 
-func initalize(conn *sqlite3.Conn) {
-  conn.Exec("CREATE TABLE feed_urls(id INT, url TEXT)")
+type FeedStore struct {
+	conn *sqlite3.Conn
 }
 
-func Open(filename string) (conn *sqlite3.Conn, err error) {
-  needToInitalize := true
-  if FileExists(filename) {
-    needToInitalize = false
-  }
-  conn, err = sqlite3.Open(filename)
-  if err != nil {
-    return conn, err
-  }
-  if needToInitalize {
-    initalize(conn)
-  }
+// Insert urlStr URL into the FeedStore
+func (fs *FeedStore) AddUrl(urlStr string) error {
+  return fs.conn.Exec("INSERT INTO feed_urls VALUES(?)", urlStr)
+}
 
-  return
+// Get the ID for a given URL
+func (fs *FeedStore) UrlId(urlStr string) int64 {
+	query := "SELECT rowid FROM feed_urls WHERE url = ?"
+  for s, err := fs.conn.Query(query, urlStr); err == nil; err = s.Next() {
+		var rowid int64
+    s.Scan(&rowid)
+    return rowid
+  }
+  return -1
+}
+
+// Get a map of stored URLs
+func (fs *FeedStore) Urls() (urls map[int64]string) {
+  urls = make(map[int64]string)
+	query := "SELECT rowid, * FROM feed_urls"
+	//row := make(sqlite3.RowMap)
+	for s, err := fs.conn.Query(query); err == nil; err = s.Next() {
+		var (
+			rowid int64
+			url   string
+		)
+    //s.Scan(&id, row)
+    s.Scan(&rowid, &url)
+    urls[rowid] = url
+	}
+
+	return urls
+}
+
+var (
+	Schema = []string{
+		"CREATE TABLE feed_urls(url TEXT)",
+		"CREATE TABLE feed_info(feed_id INT, title TEXT, lastBuildDate INT, description TEXT, link TEXT)",
+		"CREATE TABLE feed_items(feed_id INT, guid TEXT, pubDate TEXT, title TEXT, description TEXT)",
+	}
+)
+
+func initalize(conn *sqlite3.Conn) error {
+	for _, query := range Schema {
+		err := conn.Exec(query)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Open(filename string) (fs *FeedStore, err error) {
+	needToInitalize := true
+	if FileExists(filename) {
+		needToInitalize = false
+	}
+	conn, err := sqlite3.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	if needToInitalize {
+		err = initalize(conn)
+		if err != nil {
+			return nil, err
+		}
+	}
+	fs = &FeedStore{conn}
+
+	return fs, nil
 }
--- store/store_test.go
+++ store/store_test.go
@@ -1,23 +1,39 @@
 package store
 
 import (
-	"fmt"
+	"os"
 	"testing"
-  "os"
 )
 
 func TestStore(t *testing.T) {
-	c, err := Open("foo.db")
+	fs, err := Open("foo.db")
 	if err != nil {
 		t.Error(err)
 	}
-  defer os.Remove("foo.db")
-	fmt.Printf("%#v\n", c)
+	defer os.Remove("foo.db")
 
-  // open is a second time for good measure
-	c, err = Open("foo.db")
+	// open is a second time for good measure
+	fs, err = Open("foo.db")
 	if err != nil {
+		t.Logf("%#v\n", fs)
 		t.Error(err)
 	}
-	fmt.Printf("%#v\n", c)
+
+	urls := []string{
+		"http://localhost:8080/ChangeLog.rss",
+		"http://blog.hashbangbash.com/",
+	}
+	for _, url := range urls {
+		err = fs.AddUrl(url)
+		if err != nil {
+			t.Errorf("Error adding URL '%s': %s", url, err)
+		}
+		id := fs.UrlId(url)
+		if id == -1 {
+			t.Errorf("rowid for %s did not get set/fetched correctly", url)
+		}
+	}
+	if len(fs.Urls()) != 2 {
+    t.Errorf("Unexpected length of Urls %#v", fs.Urls())
+	}
 }
