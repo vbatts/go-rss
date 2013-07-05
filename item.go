7f3c854af1b82c5f8bf0d11cb6cef305fd263030
Vincent Batts <vbatts@redhat.com>
2013-07-05 18:12:58 -0400

    adding category fields and gofmt

--- item.go
+++ item.go
@@ -1,8 +1,10 @@
 package rss
 
 type Item struct {
-	Guid        string `xml:"guid"`
-	Title       string `xml:"title"`
-	PubDate     string `xml:"pubDate"`
-	Description string `xml:"description"`
+	Guid        string   `xml:"guid"`
+	Title       string   `xml:"title"`
+	Author      string   `xml:"author,omitempty"`
+	PubDate     string   `xml:"pubDate"`
+	Category    []string `xml:"category,omitempty"`
+	Description string   `xml:"description"`
 }
