07da6db790015fe47a26bdeceffacff1989cd2f9
Vincent Batts <vbatts@redhat.com>
2013-07-10 17:03:53 -0400

    adding the https://lists.fedoraproject.org/rss/announce.xml for another example RSS

--- fetch/fetch_test.go
+++ fetch/fetch_test.go
@@ -1,67 +1,95 @@
 package fetch
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"testing"
-  "io"
-  "os"
-  "time"
+	"time"
 )
 
 func init() {
 	go func() {
-		http.HandleFunc("/ChangeLog.rss", func(w http.ResponseWriter, r *http.Request) {
-      file, err := os.Open("../ChangeLog.rss")
-      if err != nil {
-        http.NotFound(w,r)
-      }
-      defer file.Close()
+		http.HandleFunc("/announce.xml", func(w http.ResponseWriter, r *http.Request) {
+			file, err := os.Open("../announce.xml")
+			if err != nil {
+				http.NotFound(w, r)
+			}
+			defer file.Close()
 
-      stat,err := file.Stat()
-      if err == nil {
-        w.Header().Set("Last-Modified", stat.ModTime().Format(time.RFC1123))
-      }
-      io.Copy(w, file)
+			stat, err := file.Stat()
+			if err == nil {
+				w.Header().Set("Last-Modified", stat.ModTime().Format(time.RFC1123))
+			}
+			io.Copy(w, file)
+		})
+		http.HandleFunc("/ChangeLog.rss", func(w http.ResponseWriter, r *http.Request) {
+			file, err := os.Open("../ChangeLog.rss")
+			if err != nil {
+				http.NotFound(w, r)
+			}
+			defer file.Close()
+
+			stat, err := file.Stat()
+			if err == nil {
+				w.Header().Set("Last-Modified", stat.ModTime().Format(time.RFC1123))
+			}
+			io.Copy(w, file)
 		})
 		log.Fatal(http.ListenAndServe(":8080", nil))
 	}()
 }
 
 func TestTime(t *testing.T) {
-  now := time.Now()
-  b, err := RemoteIsNewer("http://localhost:8080/ChangeLog.rss", now)
-  if err != nil {
-    t.Fatalf("Failed to get remote time, %s", err)
-  }
-  if b {
-    t.Errorf("this remote should not be newer than now!", b)
-  }
+	now := time.Now()
+	b, err := RemoteIsNewer("http://localhost:8080/ChangeLog.rss", now)
+	if err != nil {
+		t.Fatalf("Failed to get remote time, %s", err)
+	}
+	if b {
+		t.Errorf("this remote should not be newer than now!", b)
+	}
+}
+
+func TestFetchAnnounce(t *testing.T) {
+	r, err := FetchRss("http://localhost:8080/announce.xml")
+	if err != nil {
+		t.Fatalf("Failed to connect!, %s", err)
+	}
+	exp_str := "announce July 2013 Archive"
+	if r.Channel.Title != exp_str {
+		t.Errorf("title [%s] did not equal [%s]", r.Channel.Title, exp_str)
+	}
+	exp_int := 8
+	if len(r.Channel.Items) != exp_int {
+		t.Errorf("items [%d] did not equal [%d]", len(r.Channel.Items), exp_int)
+	}
 }
 
 func TestFetch(t *testing.T) {
-  r, err := FetchRss("http://localhost:8080/ChangeLog.rss")
-  if err != nil {
-    t.Fatalf("Failed to connect!, %s", err)
-  }
+	r, err := FetchRss("http://localhost:8080/ChangeLog.rss")
+	if err != nil {
+		t.Fatalf("Failed to connect!, %s", err)
+	}
 
 	exp_str := "SlackBuilds.org ChangeLog"
 	if r.Channel.Title != exp_str {
-		t.Errorf("title [%s] did not equal %s", r.Channel.Title, exp_str)
+		t.Errorf("title [%s] did not equal [%s]", r.Channel.Title, exp_str)
 	}
 	exp_int := 29
 	if len(r.Channel.Items) != exp_int {
-		t.Errorf("items [%d] did not equal %d", len(r.Channel.Items), exp_int)
+		t.Errorf("items [%d] did not equal [%d]", len(r.Channel.Items), exp_int)
 	}
 
-  r, err = FetchRss("http://blog.rlworkman.net/feeds/posts/default?alt=rss")
-  if err != nil {
-    t.Fatalf("Failed to connect!, %s", err)
-  }
-  if r.Channel.Title != "Roblog" {
-    t.Errorf("unexpected blog title '%s'", r.Channel.Title)
-  }
-  if len(r.Channel.Items) == 0 {
-    t.Errorf("there should be more than 0")
-  }
+	r, err = FetchRss("http://blog.rlworkman.net/feeds/posts/default?alt=rss")
+	if err != nil {
+		t.Fatalf("Failed to connect!, %s", err)
+	}
+	if r.Channel.Title != "Roblog" {
+		t.Errorf("unexpected blog title '%s'", r.Channel.Title)
+	}
+	if len(r.Channel.Items) == 0 {
+		t.Errorf("there should be more than 0")
+	}
 }
